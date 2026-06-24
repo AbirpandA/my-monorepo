f59474f28d89482bc12c8ba1d163f7b0b80ba3a9
AbirpandA <abirpanda335@gmail.com>
2026-06-24 16:26:22 +0530

    Initial commit

---
+++ apps/api/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { z } from "zod";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const userSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});     
